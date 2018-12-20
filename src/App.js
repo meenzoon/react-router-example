1c0906c1aa28865086efbc58e8f539749e8355ba
minjun kim <rlaalswns523@gmail.com>
2018-12-20 16:34:30 +0900

    use 'react-router'dom example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
+import Header from './components/Header';
+
+import Home from './routes/Home';
+import About from './routes/About';
+import Posts from './routes/Posts';
+import MyPage from './routes/MyPage';
+import Login from './routes/Login';
+import Search from './routes/Search';
+import NoMatch from './routes/NoMatch';
+
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+            <div>
+                <Header />
+                <div>
+                    {/* Switch Component 가 있으면 매칭되는 1개만 보여주는 컴포넌트 */}
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route
+                            exact
+                            path="/about/:username"
+                            component={About}
+                        />
+                        <Route path="/posts" component={Posts} />
+                        <Route path="/me" component={MyPage} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/search" component={Search} />
+                        <Route component={NoMatch} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
     );
-  }
-}
+};
 
 export default App;
---
+++ src/routes/NoMatch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoMatch = () => { 
+
+    return(
+        <div>
+            찾을 수 없는 페이지 입니다.
+        </div>
+    );
+
+}
+
+export default NoMatch;
